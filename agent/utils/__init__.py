cb91ba5b662095ba3d397d5aee631417ee24915c
sunyink <sunyink041@gmail.com>
2026-03-22 18:31:28 +0800

    impr:多存档,通过agent倒查UI日志重新实现。实现启动时自动加载、变更时节点传参或重启。 #226
    如果未来有借口,此方法留作备用。

--- agent/utils/__init__.py
+++ agent/utils/__init__.py
@@ -1,6 +1,8 @@
 # agent/utils/__init__.py
 from . import mfaalog
 from . import venv_ops
+from . import persistent_store
+from . import instance_resolver
 
 #外部调用时：必须带上文件名。
 #：utils.counter.CheckTag
